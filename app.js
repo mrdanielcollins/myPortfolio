5047829eab44e59d90b64cef076d7c070df38cd2
Daniel Collins <mrdanieljcollins@gmail.com>
2017-05-13 22:51:30 -0700

    added css file, serving html file

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+/*
+This is the main app for starting the server, functionalities include:
+Starting server, seting up routes to pages, serving html content to user
+
+Developer: Daniel Collins
+Date: 5/13/17
+*/
+
+
 //imports
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const util = require("util");
+const express = require("express");
+
+//set objects
+const app = express()
+
+//set global varibles
+var port = 5000;
+
 
 //create to web content
-var PAGES = path.join(__dirname,"public","pages");
+var PAGES = path.join(__dirname,"public");
 var CSS = path.join(__dirname,"public","css");
 var IMAGES = path.join(__dirname,"public","images");
 
@@ -15,10 +32,17 @@ console.log(`Page content location: ${PAGES}`);
 console.log(`Css content location: ${CSS}`);
 console.log(`Image content location: ${IMAGES}`);
 
-const server = http.createServer(function(res,req){
-    
-    
+//if app recieves a request for home page, serve home page
+app.get('/',function(req, res){
+    HOME_PAGE = path.join(PAGES,"home.html");
+    Style_CSS = path.join(CSS, "style.css");
+    res.sendFile(HOME_PAGE);
 });
 
+//uses files contained in public folder css,js, images...
+app.use(express.static('public'))
 
-server.listen(5000);
+// start app listening to port
+app.listen(port, function(){
+    console.log(`listening to port ${port}`);
+});
