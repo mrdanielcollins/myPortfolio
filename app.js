c51e463fc13ddc68a83ed1130e814e602a644dab
Daniel Collins <mrdanieljcollins@gmail.com>
2017-05-13 22:03:36 -0700

    updated

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var PAGES = path.join(__dirname,"public","pages");
 var CSS = path.join(__dirname,"public","css");
 var IMAGES = path.join(__dirname,"public","images");
 
+//print messages to console showing the web content paths
 console.log("Path to web content:")
 console.log(`Page content location: ${PAGES}`);
 console.log(`Css content location: ${CSS}`);
