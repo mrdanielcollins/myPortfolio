ab3c7b6c6ece473c482466c15b9100ff23ad008b
Daniel Collins <mrdanieljcollins@gmail.com>
2017-05-14 21:54:44 -0700

    started creating skills crawler

---
+++ nodes/SkillsCrawler.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var searchTerm = 'screen+scraping';
+var url = 'https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY';
+var title = 'https://www.indeed.com';
+
+var page = {
+    host: 'https://www.indeed.com',
+    port: 80,
+    path: ''
+};
+
+//request page and get all the urls
+request(url, function (err, resp, body) {
+
+    //load body of the page
+    $ = cheerio.load(body);
+
+    //jquery get all hyperlinks
+    links = $('a');
+    $(links).each(function (i, link) {
+
+        //Write each link as a text
+        console.log('***************************')
+        console.log($(link).attr('href'));
+
+        //Open links page
+
+
+        
+    });
+
+
+});
